92acdb79ff0f04bbd53663728c25b0752aac87cd
Laszlo888 <vereslaszlo34@gmail.com>
2021-07-27 18:16:31 +0100

    LoginSuccess test

--- src/test/java/MWPR_01_LoginSuccess.java
+++ src/test/java/MWPR_01_LoginSuccess.java
@@ -1,30 +1,11 @@
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class MWPR_01_LoginSuccess extends BaseTest {
-
-
-  public void login() {
-    String email = "cecej@gmail.com";
-    String password = "Asdfghj1";
-
-    driver.findElement(By.xpath("//span[contains(text(),'Sign in')]")).click();
-    wait.until(
-            ExpectedConditions.elementToBeClickable(
-                By.xpath("//input[@name='email' and @class='form-control']")))
-        .sendKeys(email);
-    driver.findElement(By.xpath("//input[@type='password']")).sendKeys(password);
-    driver.findElement(By.id("submit-login")).click();
-    wait.until(
-            ExpectedConditions.presenceOfElementLocated(By.xpath("//a[@title='View my customer account']")));
-  }
 
   @Test
   public void successfulLogin() {
 
     login();
-
 
   }
 }
