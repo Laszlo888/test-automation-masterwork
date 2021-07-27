caf7cbb641af4591dbd4371b7cfb0822af470b1a
Laszlo888 <vereslaszlo34@gmail.com>
2021-07-27 18:07:50 +0100

    RegFailed test

---
+++ src/test/java/MWPR_03_RegFailed.java
@@ -0,0 +1,30 @@
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+public class MWPR_03_RegFailed extends BaseTest {
+
+  @Test
+  public void failedRegistration() {
+
+    String firstName = "Jozsef";
+    String lastName = "Cece";
+    String email = "cecej@gmail.com";
+    String password = "Asdfghj1";
+
+    driver.findElement(By.xpath("//span[contains(text(),'Sign in')]")).click();
+    wait.until(ExpectedConditions.elementToBeClickable(By.linkText("No account? Create one here")))
+            .click();
+    wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@name='firstname']")))
+            .sendKeys(firstName);
+    driver.findElement(By.xpath("//input[@name='lastname']")).sendKeys(lastName);
+    driver.findElement(By.xpath("//input[@name='email']")).sendKeys(email);
+    driver.findElement(By.xpath("//input[@name='password']")).sendKeys(password);
+    driver.findElement(By.xpath("//input[@name='customer_privacy']")).click();
+    driver.findElement(By.xpath("//input[@name='psgdpr']")).click();
+    driver.findElement(By.xpath("//button[contains(text(),'Save')]")).click();
+    wait.until(
+            ExpectedConditions.presenceOfElementLocated(
+                    By.xpath("//li[contains(text(),'email is already used')]")));
+  }
+}
