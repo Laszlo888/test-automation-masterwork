23f87cd0e4961d4ee6d1999c960302476629e47f
Laszlo888 <vereslaszlo34@gmail.com>
2021-07-28 15:50:19 +0100

    DataChange,DataDelete created,DataSavingFromCSV modified

--- src/test/java/MWPR_08_DataSavingFromCSV.java
+++ src/test/java/MWPR_08_DataSavingFromCSV.java
@@ -1,73 +1,61 @@
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class MWPR_08_DataSavingFromCSV {
-
-  WebDriver driver;
-  WebDriverWait wait;
-
-  @BeforeEach
-  void setup() {
-    WebDriverManager.firefoxdriver().setup();
-    driver = new FirefoxDriver();
-    driver.get("http://test-automation-shop1.greenfox.academy/");
-    driver.manage().window().maximize();
-    wait = new WebDriverWait(driver, 20);
-  }
+public class MWPR_08_DataSavingFromCSV extends BaseTest {
 
   @ParameterizedTest
   @CsvFileSource(resources = "/inputAddress.csv", numLinesToSkip = 1)
   public void saveAddress(String address, String city, String postalCode) {
 
-    // login
-    String email = "cecej@gmail.com";
-    String password = "Asdfghj1";
-
-    driver.findElement(By.xpath("//span[contains(text(),'Sign in')]")).click();
-    wait.until(
-            ExpectedConditions.elementToBeClickable(
-                By.xpath("//input[@name='email' and @class='form-control']")))
-        .sendKeys(email);
-    driver.findElement(By.xpath("//input[@type='password']")).sendKeys(password);
-    driver.findElement(By.id("submit-login")).click();
-
-    // click logged in name
-    wait.until(
-            ExpectedConditions.presenceOfElementLocated(
-                By.xpath("//a[@title='View my customer account']")))
-        .click();
+    login();
 
     // click 'add first address'
-    wait.until(ExpectedConditions.elementToBeClickable(By.id("address-link"))).click();
+    wait.until(ExpectedConditions.elementToBeClickable(By.id("addresses-link"))).click();
     wait.until(ExpectedConditions.presenceOfElementLocated(By.partialLinkText("Ecommerce")));
 
-    // fill from csv
-    driver.findElement(By.xpath("//input[@name='address1']")).sendKeys(address);
-    driver.findElement(By.xpath("//input[@name='city']")).sendKeys(city);
-    driver.findElement(By.xpath("//input[@name='postcode']")).sendKeys(postalCode);
+    // decide we already have registered address or not
+    if (driver.findElement(By.xpath("//span[contains(text(),'Update')]")).isDisplayed()) {
+      driver.findElement(By.xpath("//span[contains(text(),'Update')]")).click();
+      wait.until(
+          ExpectedConditions.presenceOfElementLocated(By.xpath("//input[@name='postcode']")));
+      // fill from csv
+      driver.findElement(By.xpath("//input[@name='address1']")).clear();
+      driver.findElement(By.xpath("//input[@name='address1']")).sendKeys(address);
+      driver.findElement(By.xpath("//input[@name='city']")).clear();
+      driver.findElement(By.xpath("//input[@name='city']")).sendKeys(city);
+      driver.findElement(By.xpath("//input[@name='postcode']")).clear();
+      driver.findElement(By.xpath("//input[@name='postcode']")).sendKeys(postalCode);
 
-    Select state = new Select(driver.findElement(By.name("id_state")));
-    state.selectByVisibleText("Alaska");
+      Select state = new Select(driver.findElement(By.name("id_state")));
+      state.selectByVisibleText("Alaska");
 
-    driver.findElement(By.xpath("//button[contains(text(),'Save')]")).click();
+      driver.findElement(By.xpath("//button[contains(text(),'Save')]")).click();
 
-    wait.until(
-        ExpectedConditions.presenceOfElementLocated(
-            By.xpath(
-                "//article[@data-alert='success']/ul/li[contains(text(),'Address successfully added!')]")));
-  }
+      wait.until(
+          ExpectedConditions.presenceOfElementLocated(
+              By.xpath(
+                  "//article[@data-alert='success']/ul/li[contains(text(),'Address successfully updated!')]")));
+    } else {
+      // fill from csv
+      driver.findElement(By.xpath("//input[@name='address1']")).clear();
+      driver.findElement(By.xpath("//input[@name='address1']")).sendKeys(address);
+      driver.findElement(By.xpath("//input[@name='city']")).clear();
+      driver.findElement(By.xpath("//input[@name='city']")).sendKeys(city);
+      driver.findElement(By.xpath("//input[@name='postcode']")).clear();
+      driver.findElement(By.xpath("//input[@name='postcode']")).sendKeys(postalCode);
 
-  @AfterEach
-  public void tearDown() {
-    driver.quit();
+      Select state = new Select(driver.findElement(By.name("id_state")));
+      state.selectByVisibleText("Alaska");
+
+      driver.findElement(By.xpath("//button[contains(text(),'Save')]")).click();
+
+      wait.until(
+          ExpectedConditions.presenceOfElementLocated(
+              By.xpath(
+                  "//article[@data-alert='success']/ul/li[contains(text(),'Address successfully added!')]")));
+    }
   }
 }
---
+++ src/test/java/MWPR_09_DataChange.java
@@ -0,0 +1,50 @@
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MWPR_09_DataChange extends BaseTest {
+
+  @Test
+  public void changingLastname() {
+
+    login();
+
+    // store current lastname
+    String currentLastName =
+        driver.findElement(By.xpath("//a[@title='View my customer account']")).getText();
+
+    // click logged in name (my account)
+    driver.findElement(By.xpath("//a[@title='View my customer account']")).click();
+
+    // click 'information'
+    wait.until(ExpectedConditions.elementToBeClickable(By.id("identity-link"))).click();
+    wait.until(ExpectedConditions.presenceOfElementLocated(By.partialLinkText("Ecommerce")));
+
+    // change lastname, fill form
+    driver.findElement(By.xpath("//input[@name='lastname']")).clear();
+    driver.findElement(By.xpath("//input[@name='lastname']")).sendKeys("Cece-Kovács");
+    driver.findElement(By.xpath("//input[@name='password']")).sendKeys("Asdfghj1");
+    driver.findElement(By.xpath("//input[@name='customer_privacy']")).click();
+    driver.findElement(By.xpath("//input[@name='psgdpr']")).click();
+    driver.findElement(By.xpath("//button[contains(text(),'Save')]")).click();
+
+    wait.until(
+        ExpectedConditions.presenceOfElementLocated(
+            By.xpath("//a[@title='View my customer account']")));
+
+    // store changed lastname
+    String changedLastName =
+        driver.findElement(By.xpath("//a[@title='View my customer account']")).getText();
+
+    // store message
+    String successMessage =
+        driver.findElement(By.xpath("//article[@data-alert='success']/ul/li")).getText();
+
+    assertThat(successMessage)
+        .as("Message should be: Information successfully updated.")
+        .isEqualTo("Information successfully updated.");
+    assertThat(changedLastName).as("Lastname should be changed.").isNotEqualTo(currentLastName);
+  }
+}
