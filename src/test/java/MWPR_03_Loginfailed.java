d250c4052c5a9bf7adc213b60219ce0d69b570f1
Laszlo888 <vereslaszlo34@gmail.com>
2021-08-05 14:41:53 +0100

    LoginFailed class: name changed,styled

--- src/test/java/MWPR_03_Loginfailed.java
+++ src/test/java/MWPR_03_Loginfailed.java
@@ -5,23 +5,23 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
-public class MWPR_03_Loginfailed extends BaseTest {
+public class MWPR_03_LoginFailed extends BaseTest {
 
-    @DisplayName("Failed login")
-    @Feature("Login")
-    @Description("Do a failed login")
-    @Test
-    public void failedLogin() {
+  @DisplayName("Failed login")
+  @Feature("Login")
+  @Description("Do a failed login")
+  @Test
+  public void failedLogin() {
 
-        driver.findElement(By.xpath("//span[contains(text(),'Sign in')]")).click();
-        wait.until(
-                ExpectedConditions.elementToBeClickable(
-                        By.xpath("//input[@name='email' and @class='form-control']")))
-                .sendKeys(email);
-        driver.findElement(By.xpath("//input[@type='password']")).sendKeys(badPassword);
-        driver.findElement(By.id("submit-login")).click();
-        wait.until(
-                ExpectedConditions.presenceOfElementLocated(
-                        By.xpath("//li[contains(text(),'Authentication failed.')]")));
-    }
+    driver.findElement(By.xpath("//span[contains(text(),'Sign in')]")).click();
+    wait.until(
+            ExpectedConditions.elementToBeClickable(
+                By.xpath("//input[@name='email' and @class='form-control']")))
+        .sendKeys(email);
+    driver.findElement(By.xpath("//input[@type='password']")).sendKeys(badPassword);
+    driver.findElement(By.id("submit-login")).click();
+    wait.until(
+        ExpectedConditions.presenceOfElementLocated(
+            By.xpath("//li[contains(text(),'Authentication failed.')]")));
+  }
 }
